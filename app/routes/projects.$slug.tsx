eec45833d2112cd6378220684dcd589891582e8a
KevinJp21 <kevinjp821@gmail.com>
2024-12-09 18:37:22 -0500

    Update project localization and enhance image handling: Refactored localization files for English and Spanish to improve project descriptions and headers. Implemented dynamic image mapping for project pages using useEffect to enhance visual representation. Updated project content for clarity and consistency across languages.

--- app/routes/projects.$slug.tsx
+++ app/routes/projects.$slug.tsx
@@ -4,6 +4,12 @@ import Layout from "~/Layout/Layout";
 import { MetaFunction } from "@remix-run/node";
 import '~/content/posts/articels.css'
 import '~/Sections/Home/Projects/ToolBadge.css'
+import DocMeImg from '~/assets/Images/docme.webp'
+import ApiResidenciasImg from '~/assets/Images/ApiResidencias.webp'
+import ApiOrdersImg from '~/assets/Images/ApiOrdenes.webp'
+import ChatbotImg from '~/assets/Images/docme-chatbot.webp'
+import RestauranteSCocinaImg from '~/assets/Images/chikos-gourmet.webp'
+import { useEffect } from "react";
 
 export const meta: MetaFunction = () => {
     const { t } = useTranslation();
@@ -47,6 +53,26 @@ export default function ProjectPage() {
 
     const translationKey = getTranslationKey(slug || '');
 
+    // Mapa de imágenes
+    const projectImages: { [key: string]: string } = {
+        'P_DocMe': DocMeImg,
+        'P_APIResi': ApiResidenciasImg,
+        'P_APIOrders': ApiOrdersImg,
+        'P_Chatbot': ChatbotImg,
+        'P_RestS': RestauranteSCocinaImg
+    };
+
+    // Usar useEffect para manipular el DOM
+    useEffect(() => {
+        const projectHeader = document.querySelector('.projectHeader');
+        if (projectHeader && translationKey) {
+            projectHeader.innerHTML = `
+            <picture>
+                <img class="imgProject" itemProp="image" src="${projectImages[translationKey]}" alt="Imagen del proyecto ${slug}" width="800px" height="450px" />
+            </picture>
+        `;
+        }
+    }, [translationKey, projectImages, slug]);
 
 
     return (
@@ -58,7 +84,6 @@ export default function ProjectPage() {
                             __html: t(`${translationKey}.header`),
                         }}
                     />
-
                     <div
                         dangerouslySetInnerHTML={{
                             __html: t(`${translationKey}.content`),
